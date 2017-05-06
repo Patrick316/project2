e40ebfb0fb1c2f6409890e67ab300039c3fbf05b
Patrick316 <pattumulty@gmail.com>
2017-05-06 13:36:35 -0400

    model change

--- models/project.js
+++ models/project.js
@@ -1,12 +1,25 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var project = sequelize.define('project', {
-    name: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
+ UserName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
       }
+    },
+    Email: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      len: [1]
+    },
+    HomeAddress: {
+      type: DataTypes.STRING,
+      defaultValue: "Personal"
+    },
+      Destination: {
+      type: DataTypes.TEXT,
+      defaultValue: "Personal"
     }
   });
   return project;
