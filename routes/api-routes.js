a3ec292ec94367e909a977bac85aa45cb00966e6
Patrick316 <pattumulty@gmail.com>
2017-05-10 20:56:09 -0400

    lit

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,20 +1,27 @@
 var express = require("express");
 var router = express.Router();
 var event = require("../models/")["event"];
-
-router.get('/map', function(req, res) {
-    users.findAll()
-        .then(function(crud_data) {
-            var hbsObject = {
-                user: crud_data
-            };
-            res.render("login");
-        })
-});
+  
+  app.get("/api/event/", function(req, res) {
+    db.Post.findAll({})
+    .then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
 
 
 
-
+  // Get route for returning posts of a specific category
+  app.get("/api/event/category/:category", function(req, res) {
+    db.Post.findAll({
+      where: {
+        category: req.params.category
+      }
+    })
+    .then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
 
 
 
